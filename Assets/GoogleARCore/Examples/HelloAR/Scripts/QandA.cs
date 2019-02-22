b181534c444fd361391ad902cdecd52b67795edb
Ewaen Isoken <EwaenIsoken@gmail.com>
2019-02-22 05:52:53 +0000

    Adding all exisiting files and projects to repository. Will now be used as portfolio and back-up

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/QandA.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleARCore.Examples.HelloAR
+{
+    class QandA
+    {
+        private int questionNo;
+
+        private String question;
+
+        private double answer;
+
+        private bool correct;
+
+        QandA(int Number, String Question, double Answer, bool Correct)
+        {
+            questionNo = Number;
+            question = Question;
+            answer = Answer;
+            correct = Correct;
+        }
+
+        public void setQuestionNo (int Number)
+        {
+            questionNo = Number;
+        }
+
+        public void setQuestion (String Question)
+        {
+            question = Question;
+        }
+
+        public void setAnswer (double Answer)
+        {
+            answer = Answer;
+        }
+
+        public void setCorrect (bool Correct)
+        {
+            correct = Correct;
+        }
+
+        public int getQuestionNo()
+        {
+            return (questionNo);
+        }
+
+        public String getQuestion()
+        {
+            return (question);
+        }
+
+        public double getAnswer()
+        {
+            return (answer);
+        }
+
+        public bool getCorrect()
+        {
+            return (correct);
+        }
+    }
+}
