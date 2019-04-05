7e7bcdf5a5dbf76cd5ada69f998dbba3152dc9b8
Ewaen Isoken <EwaenIsoken@gmail.com>
2019-04-05 07:29:48 +0100

    Added Drawing Feature, Configured it. All seems to be working for now

---
+++ Assets/GoogleARCore/Examples/ScholAR/Scripts/DatabaseController.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+namespace GoogleARCore.Examples.ScholAR
+{
+    public class DatabaseController : MonoBehaviour
+    {
+        public Button SignUpButton;
+        public Button RegisterButton;
+
+        public Button LogInButton;
+        public Button ConfirmLogInButton;
+
+        public InputField LogInUsername;
+        public InputField LogInPassword;
+
+        public InputField SignUpUsername;
+        public InputField SignUpPassword;
+        public InputField ConfirmPassword;
+        public InputField SignUpEmail;
+
+        public GameObject MainMenu;
+        public GameObject SignUpMenu;
+        public GameObject LogInMenu;
+
+        private bool m_IsQuitting = false;
+
+        public void Update()
+        {
+            _UpdateApplicationLifecycle();
+
+            SignUpButton.onClick.AddListener(SignUpHandler);
+            FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://scholar-ac37c.firebaseio.com/");
+            LogInButton.onClick.AddListener(LogInHandler);
+        }
+
+        public void TakeInfo()
+        {
+            LogInButton.onClick.AddListener(LogInHandler);
+        }
+
+        public void SignUpHandler()
+        {
+            MainMenu.SetActive(false);
+            SignUpMenu.SetActive(true);
+
+            RegisterButton.onClick.AddListener(CheckSignUp);
+
+        }
+
+        private void LogInHandler()
+        {
+            MainMenu.SetActive(false);
+            LogInMenu.SetActive(true);
+
+            ConfirmLogInButton.onClick.AddListener(CheckLogIn);
+        }
+
+        public void CheckSignUp()
+        {
+            string Username = SignUpUsername.text;
+            string Password = SignUpPassword.text;
+            string confirmPassword = ConfirmPassword.text;
+            string email = SignUpEmail.text;
+            string userid = "";
+
+            if (Username.Equals(null))
+            {
+                _ShowAndroidToastMessage("Please Enter a Username");
+                SignUpHandler();
+            }
+
+            if (Password.Equals(null))
+            {
+                _ShowAndroidToastMessage("Please Enter a Password");
+                SignUpHandler();
+            }
+
+            if (email.Equals(null))
+            {
+                _ShowAndroidToastMessage("Please Enter an Email");
+                SignUpHandler();
+            }
+
+            if(!email.Contains("@"))
+            {
+                _ShowAndroidToastMessage("Please enter a valid email");
+                SignUpHandler();
+            }
+
+            if (!Password.Equals(confirmPassword))
+            {
+                _ShowAndroidToastMessage("The passwords do not match");
+                Debug.Log("The passwords do not match");
+            }
+            else
+            {
+                User user = new User(userid, Username, Password, email);
+                FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://scholar-ac37c.firebaseio.com/");
+                //DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+                WriteNewUser(user);
+            }
+        }
+
+        public void CheckLogIn()
+        {
+            bool isLoggedIn;
+            string Username = LogInUsername.text;
+            string Password = LogInPassword.text;
+            string key;
+            //DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+            //DatabaseReference data = reference
+            FirebaseDatabase database = FirebaseDatabase.GetInstance("https://scholar-ac37c.firebaseio.com");
+            DatabaseReference reference = database.GetReference("Users");
+            Query query = reference.OrderByChild("Username").EqualTo(Username.ToString());
+            //query.ValueChanged()
+            //if (Username.Equals(reference.OrderByChild(Username)) && Password)#
+
+            FirebaseDatabase.DefaultInstance.GetReference("Users").OrderByChild(Username).GetValueAsync().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    //_ShowAndroidToastMessage("Incorrect Log-in information");
+                }
+                else if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    if (Password.Equals(snapshot.Child("Password").GetRawJsonValue()))
+                    {
+                        isLoggedIn = true;
+                        key = snapshot.Child("userId").GetRawJsonValue();
+                    }
+                }
+            });
+        }
+
+        private void WriteNewUser(User user)
+        {
+            FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://scholar-ac37c.firebaseio.com/");
+            DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+            string key = reference.Child("Users").Push().Key;
+            user.SetUserId(key);
+            string json = JsonUtility.ToJson(user);
+            reference.Child("Users").Child(key).SetRawJsonValueAsync(json);
+
+            _ShowAndroidToastMessage("Sign Up successful! Please log-in with your new details");
+            SignUpMenu.SetActive(false);
+            MainMenu.SetActive(true);
+            Update();
+        }
+
+        private void _UpdateApplicationLifecycle()
+        {
+            // Exit the app when the 'back' button is pressed.
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            // Only allow the screen to sleep when not tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                const int lostTrackingSleepTimeout = 15;
+                Screen.sleepTimeout = lostTrackingSleepTimeout;
+            }
+            else
+            {
+                Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            }
+
+            if (m_IsQuitting)
+            {
+                return;
+            }
+
+            // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+            if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+            {
+                _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+            else if (Session.Status.IsError())
+            {
+                _ShowAndroidToastMessage("ARCore encountered a problem connecting. Please start the app again.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+        }
+
+        /// <summary>
+        /// Actually quit the application.
+        /// </summary>
+        private void _DoQuit()
+        {
+            Application.Quit();
+        }
+
+        void _ShowAndroidToastMessage(string message)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityActivity != null)
+            {
+                AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+                unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                {
+                    AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                        message, 0);
+                    toastObject.Call("show");
+                }));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TrailScript2.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+#if UNITY_EDITOR
+// Set up touch input propagation while using Instant Preview in the editor.
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+
+public class TrailScript2 : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    public GameObject camera;
+
+    public List<GameObject> Points = new List<GameObject>();
+
+    bool resume = false;
+
+    private bool m_IsQuitting = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        if (Input.GetMouseButton(0) || Input.touchCount > 0)
+        {
+            Debug.Log("Touched");
+            Vector3 camPos = camera.transform.position;
+            Vector3 camDirection = camera.transform.forward;
+            Quaternion camRotation = camera.transform.rotation;
+            float spawnDistance = 2;
+            Debug.Log("Touched" + camPos.x + " " + camPos.y + " " + camPos.z);
+            Vector3 spawnPos = camPos + (camDirection * spawnDistance);
+            GameObject cur = Instantiate(gameObject, spawnPos, camRotation);
+            cur.transform.SetParent(this.transform);
+        }
+    }
+
+
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                    message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting. Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+}
